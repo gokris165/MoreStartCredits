a894452bfb1929d4763deac1251dabb5e42db83d
Gokul Krishna <gokris165@gmail.com>
2024-05-03 19:35:09 -0700

    working version of mod without a config file, basically hardcoded

---
+++ Patches/StartCreditPatch.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreStartCredits.Patches
+{
+    [HarmonyPatch(typeof(StartOfRound))]
+    internal class StartCreditPatch
+    {
+        internal static ManualLogSource logger; 
+        
+
+        [HarmonyPatch("SetTimeAndPlanetToSavedSettings")]
+        [HarmonyPostfix]
+        static void changeStartingCredits(ref EndOfGameStats ___gameStats, ref bool ___isChallengeFile)
+        {
+            logger = BepInEx.Logging.Logger.CreateLogSource("Brocool.MoreStartCredits");
+            logger.LogWarning("Beginning the change");
+
+            try
+            {
+                if (___gameStats.daysSpent == 0 && !___isChallengeFile)
+                {
+                    TimeOfDay.Instance.quotaVariables.startingCredits = 300;
+                }
+            }
+            catch(Exception e)
+            { 
+                logger.LogWarning("FAILED: Brocool.MoreStartCredits");
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using MoreStartCredits.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreStartCredits
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string modGUID = "Brocool.MoreStartCredits";
+        private const string modName = "MoreStartCredits";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static Plugin Instance;
+        internal ManualLogSource logger;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            logger = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            logger.LogWarning(modName + " has started!");
+
+            harmony.PatchAll();
+        }
+
+    }
+}
